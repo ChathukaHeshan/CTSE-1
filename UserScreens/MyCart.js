4066ba0d772eaa4cc8ec25cee9baaf1e73c63f34
shaiu98 <87427436+shaiu98@users.noreply.github.com>
2023-03-26 15:56:32 +0530

    Update MyCart.js

--- UserScreens/MyCart.js
+++ UserScreens/MyCart.js
@@ -356,7 +356,7 @@ const styles = StyleSheet.create({
     },
     priceText: {
         paddingTop: 30,
-        fontSize: 16,
+        
         color: "#fff",
         fontWeight: "500",
         width: 60,
